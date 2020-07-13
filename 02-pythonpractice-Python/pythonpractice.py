cce3a5aa0b58afd059e28007608b7e97450e3c8a
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-13 19:23:42 +0530

    2nd Python Code

--- 02-pythonpractice-Python/pythonpractice.py
+++ 02-pythonpractice-Python/pythonpractice.py
@@ -18,3 +18,33 @@ Use the test cases below to guide you!"""
 class Classy(object):
     def __init__(self):
         self.items = []
+
+    def addItem(self, s):
+        self.items.append(s)
+    
+    def classiness(self):
+        score = 0
+        for i in self.items:
+            if (i == "tophat"):
+                score = score + 2
+            elif (i == "bowtie"):
+                score = score + 4
+            elif (i == "monocle"):
+                score = score + 5
+        return score    
+
+# def main():
+#     me = Classy()
+#     result = me.classiness()
+#     me.addItem("tophat")
+#     result = me.classiness()
+#     me.addItem("bowtie")
+#     me.addItem("jacket")
+#     me.addItem("monocle")
+#     result = me.classiness()
+#     me.addItem("bowtie")
+#     result = me.classiness()
+#     print(result)
+
+# if __name__ == "__main__":
+#     main()
