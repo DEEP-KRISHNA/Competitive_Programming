72397a61a772c99eee8b28278ae0e053cfc4f035
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-14 17:37:13 +0530

    Jul 14  17:37:13

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -9,10 +9,14 @@ import math
 
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	d1 = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
-	d2 = math.sqrt(math.pow(x3 - x2, 2) + math.pow(y3 - y2, 2))
-	d3 = math.sqrt(math.pow(x3 - x1, 2) + math.pow(y3 - y1, 2))
+	d1 = math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2)
+	d2 = math.pow(x3 - x2, 2) + math.pow(y3 - y2, 2)
+	d3 = math.pow(x3 - x1, 2) + math.pow(y3 - y1, 2)
 	dist = [d1, d2, d3]
 	dist.sort()
-	return int(math.pow(d3, 2)) == int(math.pow(d1, 2) + math.pow(d2, 2))
+	print(dist)
+	return math.isclose(dist[0], dist[1] + dist[2])
 	# pass
+
+if __name__ == "__main__":
+	print(isrighttriangle(13, -1, -9, 3, -3, -9))
