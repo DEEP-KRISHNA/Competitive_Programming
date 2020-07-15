b4c9825183984cae0e17c7ad2cf461f131473ada
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-15 10:01:12 +0530

    18th code

--- 18-lineintersection-Python/lineintersection.py
+++ 18-lineintersection-Python/lineintersection.py
@@ -12,7 +12,8 @@ def lineintersection(m1, b1, m2, b2):
 		return None
 	elif (m2 % m1 == 0):
 		return None
-	return b2-b1/m1-m2
+	# print(b2,b1,m1,m2)
+	return int((b2-b1)/(m1-m2))
 	# pass
 
 if __name__ == "__main__":
