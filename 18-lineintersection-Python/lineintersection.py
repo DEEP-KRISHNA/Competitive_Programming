9f1221465d864e6d3bc83c602992a0a6aab49ee0
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-15 09:58:54 +0530

    Jul 15  09:58:54

--- 18-lineintersection-Python/lineintersection.py
+++ 18-lineintersection-Python/lineintersection.py
@@ -8,5 +8,12 @@ def lineintersection(m1, b1, m2, b2):
 	# your code goes here
 	if (m1 == m2):
 		return None
+	elif (m1 % m2 == 0):
+		return None
+	elif (m2 % m1 == 0):
+		return None
 	return b2-b1/m1-m2
-	pass
+	# pass
+
+if __name__ == "__main__":
+	print(lineintersection(4, 13, 3, 17))
