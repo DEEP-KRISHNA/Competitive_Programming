3976f08366e43f231477ab0464f292e0b575cc05
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-15 20:16:28 +0530

    Jul 15  20:16:28

--- 03-recursion-Python/recursion.py
+++ 03-recursion-Python/recursion.py
@@ -8,9 +8,11 @@ def get_fib(position):
         return 1
     elif (position == 2):
         return 1
+    elif (position == 0):
+        return 0
     else:
         return get_fib(position-1) + get_fib(position-2)
     # return -1
 
 if __name__ == "__main__":
-    print(get_fib(9))
+    print(get_fib(0))
