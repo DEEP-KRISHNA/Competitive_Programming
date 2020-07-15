f3cf0a0b57d679d61ed64b5fb88d9eccb1529e48
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-15 21:43:53 +0530

    06-get_kth_digit-Python

--- 06-get_kth_digit-Python/get_kth_digit.py
+++ 06-get_kth_digit-Python/get_kth_digit.py
@@ -8,8 +8,16 @@ import math
 
 def fun_get_kth_digit(digit, k):
 	# return 0
-	pow = math.pow(10,k)
-	rem = digit % pow
-	pow = math.pow(10, k - 1)
-	dig = rem / pow
-	return dig
+	if (digit < 0):
+		digit = digit * -1
+	digit = str(digit)
+	if (k >= len(digit)):
+		return 0
+	else:
+		return int(digit[len(digit) - k - 1])
+
+if __name__ == "__main__":
+	for i in [(789, 0, 9), (789, 1, 8), (789, 2, 7), (789, 3, 0), (-789, 0, 9), (-780, 4, 0)]:
+		if (fun_get_kth_digit(i[0], i[1]) != i[2]):
+			print(fun_get_kth_digit(i[0], i[1]))
+			print(i)
