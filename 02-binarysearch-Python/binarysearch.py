627d5511b053eef5f4b1e3f9d12db8cff4bd7804
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-18 00:04:50 +0530

    Jul 18  00:04:50

--- 02-binarysearch-Python/binarysearch.py
+++ 02-binarysearch-Python/binarysearch.py
@@ -10,6 +10,7 @@ elements are in a strictly increasing order.
 Return the index of value, or -1 if the value
 doesn't exist in the list."""
 
-def binary_search(input_array, value):
+def binary_search(array, value):
     # Your code goes here
+    
     pass
