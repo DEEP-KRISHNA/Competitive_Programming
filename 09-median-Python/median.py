a8de437383e0c64b230f2e0119e7ee258b8ceedd
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-17 18:41:19 +0530

    09-median-Python

--- 09-median-Python/median.py
+++ 09-median-Python/median.py
@@ -5,4 +5,13 @@
 
 def median(a):
 	# your code goes here
-	pass
+	if (len(a) == 0):
+		return None
+	elif (len(a) % 2 == 0):
+		pos = int(len(a) / 2)
+		return (a[pos] + a[pos-1])/2
+	else:
+		return a[int(len(a) / 2)]
+
+if __name__ == "__main__":
+	print(median([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
