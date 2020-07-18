87db059583aa5941344a2a0887aa2ec52e788636
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-18 19:39:47 +0530

    03-quicksort-Python

--- 03-quicksort-Python/quicksort.py
+++ 03-quicksort-Python/quicksort.py
@@ -3,7 +3,8 @@ Input a list.
 Output a sorted list."""
 def quicksort(array):
 	# Your code goes here
-	return array.sort()
+	array.sort()
+	return array
 
 if __name__ == "__main__":
 	print(quicksort([21, 4, 1, 3, 9, 20, 25, 6, 21, 14]))
