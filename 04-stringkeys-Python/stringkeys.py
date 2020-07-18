ea302c32c31c23017adad2c3038c044ff08bd4af
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-18 19:50:28 +0530

    Jul 18  19:50:28

--- 04-stringkeys-Python/stringkeys.py
+++ 04-stringkeys-Python/stringkeys.py
@@ -4,21 +4,25 @@ using the first two letters of the string."""
 
 class HashTable(object):
     def __init__(self):
-        self.table = [None]*10000
+        self.table = [-1]*10000
 
     def store(self, string):
         """Input a string that's stored in 
         the table."""
         # Hash Value = (ASCII Value of First Letter * 100) + ASCII Value of Second Letter 
         # Your code goes here
-        pass
+        hashv = self.calculate_hash_value(string)
+        self.table[hashv] = string
+        # pass
         
     def lookup(self, string):
         """Return the hash value if the
         string is already in the table.
         Return -1 otherwise."""
         # Your code goes here
-        pass
+        hashv = self.calculate_hash_value(string)
+        return(self.table[hashv])
+        # pass
 
     def calculate_hash_value(self, string):
         """Helper function to calulate a
@@ -26,5 +30,13 @@ class HashTable(object):
         # Your code goes here
         return ord(string[0])*100 + ord(string[1])
         # pass
+
+if __name__ == "__main__":
+    hash_table = HashTable()
+    print(hash_table.calculate_hash_value('UDACITY'))
+    print(hash_table.lookup('UDACITY'))
+    print(hash_table.lookup('UDACITY'))
+    print(hash_table.calculate_hash_value('UDACIOUS'))
+    
 
 
