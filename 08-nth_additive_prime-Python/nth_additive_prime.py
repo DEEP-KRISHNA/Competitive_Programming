f72bc69e49a68a9637da3f1224408c613018396a
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-20 16:33:48 +0530

    08-nth_additive_prime-Python

--- 08-nth_additive_prime-Python/nth_additive_prime.py
+++ 08-nth_additive_prime-Python/nth_additive_prime.py
@@ -7,4 +7,22 @@
 
 
 def fun_nth_additive_prime(n):
-	return 1
+	# return 1
+	i = 2
+	n_i = -1
+	while (n_i != n):
+		if (isPrime(i)):
+			itemp = str(i)
+			summ = 0
+			for j in itemp:
+				summ = summ + int(j)
+			if (isPrime(summ)):
+				n_i += 1
+		i += 1
+	return i -1
+
+def isPrime(p):
+	for i in range(2, int(p/2) + 1):
+		if (p % i == 0):
+			return False
+	return True
