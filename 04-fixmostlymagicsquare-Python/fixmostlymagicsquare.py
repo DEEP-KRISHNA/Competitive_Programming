c7288fc471c94c310112144422d77873c31cabe5
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-20 23:04:23 +0530

    04-fixmostlymagicsquare-Python

--- 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
+++ 04-fixmostlymagicsquare-Python/fixmostlymagicsquare.py
@@ -10,12 +10,32 @@ def fixmostlymagicsquare(L):
 	# pass
 	# Your code goes here
 	dic = {}
-	for i in range(len(a[0])):
+	for i in range(len(L[0])):
 		summ1 = 0
 		summ2 = 0
-		for j in range(len(a[0])):
-			summ1 = summ1 + a[i][j]
-			summ2 = summ2 + a[j][i]
-		dic[(i,0)] = summ1
-		dic[(0,i)] = summ2
-	lst.sort()
+		for j in range(len(L[0])):
+			summ1 = summ1 + L[i][j]
+			summ2 = summ2 + L[j][i]
+		dic[(i,"L")] = summ1
+		dic[(i, "H")] = summ2
+	val = list(dic.values())
+	val.sort()
+	commonval = val[int(len(val) / 2)]
+	x = -1
+	y = -1
+	excessval = -1
+	for i in dic:
+		if (dic[i] != commonval):
+			if (i[1] == 'L'):
+				x = i[0]
+			else:
+				y = i[0]
+			excessval = dic[i] - commonval
+	L[x][y] = L[x][y] - excessval
+	return L
+		
+	
+
+if __name__ == "__main__":
+	print(fixmostlymagicsquare(
+		[[16, 3, 2, 13], [5, 10, 11, 18], [9, 6, 7, 12], [4, 15, 14, 1]]))
