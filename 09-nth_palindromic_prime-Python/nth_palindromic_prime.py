cbbcb9501a56fc83c03783d14deebb06fabcc195
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-20 16:40:09 +0530

    def fun_nth_additive_prime(n):

--- 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
+++ 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
@@ -7,4 +7,20 @@
 
 
 def fun_nth_palindromic_prime(n):
-	return 0
+	# return 1
+	i = 2
+	n_i = -1
+	while (n_i != n):
+		if (isPrime(i)):
+			itemp = str(i)
+			if(itemp == itemp[::-1]):
+				n_i += 1
+		i += 1
+	return i - 1
+
+
+def isPrime(p):
+	for i in range(2, int(p/2) + 1):
+		if (p % i == 0):
+			return False
+	return True
