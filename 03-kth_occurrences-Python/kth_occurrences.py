15965fa70ab123fbb10c76ee4d05697e6a192cd4
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-20 18:27:37 +0530

    Jul 20  18:27:37

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -5,6 +5,19 @@
 
 
 def fun_kth_occurrences(s, n):
+	dic = {}
+	for i in s:
+		if i in dic.keys():
+			dic[i] = dic[i] + 1
+		else:
+			dic[i] = 1
+	lst = list(dic.values())
+	lst.sort()
+	lst = lst[::-1]
+	print(lst[n])
 	return 'a'
+
+if __name__ == "__main__":
+	fun_kth_occurrences("helllo woorld", 2)
 
 
