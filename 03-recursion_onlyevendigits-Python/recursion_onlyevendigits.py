a397f36923c6c56a75c93f2b0a894bb2d410d253
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-22 13:23:38 +0530

    03-recursion_onlyevendigits-Python

--- 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
+++ 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
@@ -8,5 +8,23 @@
 # Also the function returns the empty list if the original list is empty. 
 # Remember to not use strings. You may not use loops/iteration in this problem.
 
-def fun_recursion_onlyevendigits(l): 
+def fun_recursion_onlyevendigits(lst):
+	if (len(lst) == 0):
 		return []
+	newlst = []
+	for i in lst:
+		newlst.append(rec(str(i),'',0))
+	return newlst
+def rec(numm, res, i):
+	if (i == len(numm)):
+		if res == '':
+			return 0
+		return int(res)
+	else:
+		if ((int(numm[i]) % 2) == 0):
+			return rec(numm, res + numm[i], i + 1)
+		else:
+			return rec(numm, res, i + 1)
+
+if __name__ == "__main__":
+	print(fun_recursion_onlyevendigits([43, 23265, 17, 58344]))
