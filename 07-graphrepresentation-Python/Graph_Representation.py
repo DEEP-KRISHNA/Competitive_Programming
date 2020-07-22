2b28825f8f6a415c3f5d10d6752562b990e32106
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-22 17:13:23 +0530

    Jul 22  17:13:23

--- 07-graphrepresentation-Python/Graph_Representation.py
+++ 07-graphrepresentation-Python/Graph_Representation.py
@@ -38,14 +38,19 @@ class Graph(object):
         self.edges.append(new_edge)
 
     def get_edge_list(self):
+        dic = {}
         for i in self.nodes:
-            print(i.value)
+            val = i.value
             for j in i.edges:
-                print(j.value)
-                print(j.node_from)
-                print(j.node_to)
-            print()
+                if j.value in dic.keys():
+                    dic[j.value].append(val)
+                else:
+                    dic[j.value] = [val]
         edge_list = []
+        for i in dic:
+            lst = [i]
+            lst.extend(dic[i])
+            edge_list.append(tuple(lst))
         return edge_list
 
     def get_adjacency_list(self):
@@ -54,7 +59,14 @@ class Graph(object):
     
     
     def get_adjacency_matrix(self):
+        max_index = len(self.edges) + 1
+        # print(len(self.nodes))
         adjacency_matrix = [[0 for i in range(max_index + 1)] for j in range(max_index + 1)]
+        edg_lst = self.get_edge_list()
+        # print(edg_lst)
+        for i in edg_lst:
+            adjacency_matrix[i[1]][i[2]+1] = i[0]
+        print(adjacency_matrix)
         return adjacency_matrix
 
 
@@ -64,4 +76,5 @@ if __name__ == "__main__":
     graph.insert_edge(101, 1, 3)
     graph.insert_edge(102, 1, 4)
     graph.insert_edge(103, 3, 4)
-    print(graph.get_edge_list())
+    # print(graph.get_edge_list())
+    print(graph.get_adjacency_matrix())
