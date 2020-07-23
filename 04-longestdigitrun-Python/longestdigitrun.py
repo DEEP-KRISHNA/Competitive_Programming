8213f75e16d2ed86ba20d1509934821469ad8e1a
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-23 16:38:57 +0530

    Jul 23  16:38:57

--- 04-longestdigitrun-Python/longestdigitrun.py
+++ 04-longestdigitrun-Python/longestdigitrun.py
@@ -5,5 +5,34 @@
 # because there is a run of 3 consecutive 7's), 
 # as does longestDigitRun(-677886).
 def longestdigitrun(n):
+	if (n < 0):
+		n = n * -1
+	lst = []
+	for i in str(n):
+		lst.append(i)
+	look = lookandsay(lst)
 	# Your code goes here
 	pass
+
+
+def lookandsay(a):
+	maxx = 0
+	count = 1
+	lst = []
+	if (len(a) == 0):
+		return (lst,maxx)
+	for i in range(1, len(a)):
+		# print(a[i], a[i-1])
+		if (a[i] == a[i - 1]):
+			count = count + 1
+		else:
+			if (count > maxx):
+				maxx = count
+			lst.append((count, a[i-1]))
+			count = 1
+	lst.append((count, a[i]))
+	return (lst,maxx)
+
+
+if __name__ == "__main__":
+	print(lookandsay([-1, 2, 7]))
