1b98027198fa9664ddaf96952cfca8fa3ba580ab
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-25 14:59:33 +0530

    Jul 25  14:59:33

--- 08-nth_circularprime-Python/nthcircularprime.py
+++ 08-nth_circularprime-Python/nthcircularprime.py
@@ -7,24 +7,42 @@
 
 import math
 
+global dic
+dic = {}
+global maxkey
+maxkey = 0
+
 def nthcircularprime(n):
-	# n = n + 1
-	i = 0
-	num = 1
-	while (i < n):
-		nu = str(num)
-		flag = True
-		for j in range(len(nu)):
-			left = nu[j:]
-			right = nu[:j]
-			# print(num,left+right)
-			if (not prime(int(left + right))):
-				flag = False
-		if (flag):
-			i = i + 1
-		# print(i,num)
-		num = num + 1
-	return num - 1
+	global maxkey
+	global dic
+	n = n + 1
+	if (n <= maxkey):
+		print(dic)
+		return dic[n]
+	else:
+		if (maxkey == 0):
+			i = 0
+			num = 1
+		else:
+			i = maxkey-1
+			num = dic[maxkey]-1
+		while (i < n):
+			nu = str(num)
+			if('0' not in nu):
+				flag = True
+				for j in range(len(nu)):
+					left = nu[j:]
+					right = nu[:j]
+					# print(num,left+right)
+					if (not prime(int(left + right))):
+						flag = False
+				if (flag):
+					i = i + 1
+					dic[i] = num
+					maxkey = i
+				# print(i,num)
+			num = num + 1
+		return num - 1
 
 def prime(num):
 	if (num <= 1):
@@ -35,4 +53,4 @@ def prime(num):
 	return True
 
 if __name__ == "__main__":
-	print(nthcircularprime(5))
+	print(nthcircularprime(46))
