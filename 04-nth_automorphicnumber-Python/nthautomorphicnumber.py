80e2f478cb3096084ee494c69d1713eddd6bbd59
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-25 20:51:21 +0530

    Jul 25  20:51:21

--- 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
+++ 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
@@ -3,6 +3,45 @@
 # number itself. For example, 5^2 = 25, 6^2 = 36, 76^2 = 5776, and 890625^2 = 793212890625, so 5, 6, 
 # 76 and 890625 are all automorphic numbers.
 
+import math
+
+global dic
+dic = {}
+global maxkey
+maxkey = 0
+
 def nthautomorphicnumbers(n):
-	# Your code goes here
-	pass
+	global maxkey
+	global dic
+	n = n + 1
+	if (n <= maxkey):
+		return dic[n]
+	else:
+		if (maxkey == 0):
+			i = 0
+			num = 1
+		else:
+			i = maxkey
+			num = dic[maxkey] + 1
+		# while (i < n):
+		# 	nu = num
+		# 	for i in range(len(str(nu))):
+
+
+def mul(num):
+	nu = num
+	lenn = len(str(nu))
+	summ = 0
+	lst1 = []
+	lst2 = []
+	for i in range(lenn):
+		den = 10 ** (lenn - i - 1)
+		lst1.append(int(nu / den))
+		lst2.append(nu)
+		# summ = summ + (nu * int(nu / den))
+		nu = nu % den
+	print(lst1,lst2)
+
+if __name__ == "__main__":
+	mul(9376)
+
