80a426003ac72f9ea74d810c9872b84c81cb87b5
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-25 14:44:33 +0530

    Jul 25  14:44:33

--- 08-nth_circularprime-Python/nthcircularprime.py
+++ 08-nth_circularprime-Python/nthcircularprime.py
@@ -8,19 +8,21 @@
 import math
 
 def nthcircularprime(n):
-	n = n
+	# n = n + 1
 	i = 0
 	num = 1
 	while (i < n):
 		nu = str(num)
 		flag = True
-		for i in range(len(nu)):
-			left = nu[i:]
-			right = nu[:i]
+		for j in range(len(nu)):
+			left = nu[j:]
+			right = nu[:j]
+			# print(num,left+right)
 			if (not prime(int(left + right))):
 				flag = False
 		if (flag):
 			i = i + 1
+		# print(i,num)
 		num = num + 1
 	return num - 1
 
@@ -28,9 +30,9 @@ def prime(num):
 	if (num <= 1):
 		return False
 	for i in range(2, int(math.sqrt(num)) + 1):
-		if (num % 1 == 0):
+		if (num % i == 0):
 			return False
 	return True
 
 if __name__ == "__main__":
-	print(nthcircularprime(0))
+	print(nthcircularprime(2))
