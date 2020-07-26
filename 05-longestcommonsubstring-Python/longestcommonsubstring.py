828067edad748526f6dd4e5c223a153176dc00dd
DEEP-KRISHNA <sandy.krish1995@msitprogram.net>
2020-07-26 22:59:55 +0530

    Jul 26  22:59:55

--- 05-longestcommonsubstring-Python/longestcommonsubstring.py
+++ 05-longestcommonsubstring-Python/longestcommonsubstring.py
@@ -9,4 +9,27 @@
 
 def longestcommonsubstring(s1, s2):
     # Yourcode goes here
+    if ((len(s1) == 0) or (len(s2) == 0)):
+        return ""
+    if (len(s2) > len(s1)):
+        temp = s1
+        s1 = s2
+        s2 = temp
+    lst = []
+    for i in range(1,len(s1)):
+        j = 0
+        flag = False
+        while (j < len(s1)):
+            temp = s1[j: j + i]
+            if (len(temp) == i):
+                if(temp in s2):
+                    if (not flag):
+                        lst = []
+                        flag = True
+                    lst.append(temp)
+            j = j + 1
+    print(lst)
     pass
+
+if __name__ == "__main__":
+    longestcommonsubstring("abcABC", "zzabZZAB")
